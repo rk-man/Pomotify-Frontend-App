a5e312693b63abef8b162f779804ffc75f0f6eb7
rk_man <naveenbadi2003@gmail.com>
2023-01-20 17:24:11 +0530

    New changes

--- config/index.js
+++ config/index.js
@@ -4,4 +4,4 @@ export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 
 export const REDIRECT_URI = process.env.REDIRECT_URI || "http://localhost:3000";
 
-export const SPOTIFY_AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${process.env.CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
+export const SPOTIFY_AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${process.env.CLIENT_ID}&response_type=code&redirect_uri=https://pomotifyrk.vercel.app/spotify&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
